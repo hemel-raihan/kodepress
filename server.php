850c59a38682554a4b14999faa9ae05864d6222e
Rakibul8001 <ghumpori8001@gmail.com>
2021-12-05 11:49:13 +0600

    fixing .htaccess problem

--- server.php
+++ server.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
-Project         :   Zanex - Laravel Admin & Dashboard Template
-@package        :   Laravel
-Laravel Version :   8.54
-PHP Version     :   8.0.9
-Create Date     :   31/08/2021
-Copyright       :   Spruko Technologies Private Limited
-Author          :   Spruko
-Author URL      :   https://themeforest.net/user/sprukosoft
-Support         :   support@spruko.com
-License         :   Licensed under ThemeForest Licence
-*/
-
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
